a5017ba1d584b0503d7fc8ab208cc2c5ed954f02
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-06-28 06:39:13 +0530

    re-render optimization

--- Week-6/react-basics/src/App.jsx
+++ Week-6/react-basics/src/App.jsx
@@ -1,10 +1,31 @@
+import React,{Fragment, useState} from "react";
 function App() {
+
   return (
-    <>
-      <h3>Hello</h3>
-      <h4>Hi</h4>
-    </>
-  );
+   <>
+      <RenderingHeader></RenderingHeader>
+      <Header title="harkirat"></Header>
+      <Header title="Ramesh"></Header>
+   </>
+  )
+}
+function RenderingHeader(){
+
+  const [title,setTitle]=useState("Ram");
+
+  function updateText(){
+    setTitle("My Name Is "+Math.random());
+  }
+  
+  return( 
+  <div>
+  <button onClick={updateText}>UpdateData</button>
+  <Header title={title}></Header>
+  </div>)
+}
+
+function Header(props){
+  return <div>{props.title}</div>
 }
 
 export default App;
