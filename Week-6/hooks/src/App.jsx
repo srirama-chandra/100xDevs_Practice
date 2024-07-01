a258dba4b65c6284cacf6524e75307344efc3e79
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-07-01 11:56:14 +0530

    useEffect-2

--- Week-6/hooks/src/App.jsx
+++ Week-6/hooks/src/App.jsx
@@ -1,22 +1,29 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(()=>{
-    axios.get("https://sum-server.100xdevs.com/todos").then((res)=>setTodos(res.data.todos));
-  },[]);
+  const [number, setNumber] = useState(1);
 
   return (
     <div>
-        {todos.map((e)=> <Todo title={e.title} description={e.description}></Todo>)}
+      <button onClick={()=>setNumber(1)}>1</button>
+      <button onClick={()=>setNumber(2)}>2</button>
+      <button onClick={()=>setNumber(3)}>3</button>
+      <Todo id={number}></Todo>
     </div>
-  )
+  );
 }
 
-function Todo({title,description}){
-    return (<div><h1>{title}</h1><p>{description}</p></div>)
+function Todo({id}) {
+  const [todo,setTodo]=useState({});
+  useEffect(()=>{axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`).then((e)=>setTodo(e.data.todo))},[id])
+
+  return (
+    <div>
+      <h1>{todo.title}</h1>
+      <p>{todo.description}</p>
+    </div>
+  );
 }
-export default App
+export default App;
