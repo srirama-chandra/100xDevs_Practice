d418fbc622f7c24c38a53f2c8545d1bfc9d5bd7a
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-07-11 11:53:22 +0530

    Revision

--- Week-6/hooks/src/App.jsx
+++ Week-6/hooks/src/App.jsx
@@ -5,25 +5,28 @@ import "./App.css";
 function App() {
   const [number, setNumber] = useState(1);
 
-  return (
+  return(
     <div>
-      <button onClick={()=>setNumber(1)}>1</button>
-      <button onClick={()=>setNumber(2)}>2</button>
-      <button onClick={()=>setNumber(3)}>3</button>
+      <button onClick={()=>{setNumber(1)}}>1</button>
+      <button onClick={()=>{setNumber(2)}}>2</button>
+      <button onClick={()=>{setNumber(3)}}>3</button>
       <Todo id={number}></Todo>
     </div>
-  );
+  )
+  
 }
 
 function Todo({id}) {
-  const [todo,setTodo]=useState({});
-  useEffect(()=>{axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`).then((e)=>setTodo(e.data.todo))},[id])
+  const [todos,setTodos]=useState({});
+
+  useEffect(()=>{axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`).then(res=>setTodos(res.data.todo))},[id])
 
   return (
     <div>
-      <h1>{todo.title}</h1>
-      <p>{todo.description}</p>
+      <h1>{todos.title}</h1>
+      <p>{todos.description}</p>
     </div>
-  );
+  )
+
 }
 export default App;
