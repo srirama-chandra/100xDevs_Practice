881e38bbadd854bfde8cd7bb25d7ca0510f869ab
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-07-20 15:24:15 +0530

    useNavigate

--- Week-7/react-routing/src/App.jsx
+++ Week-7/react-routing/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route,Routes } from 'react-router-dom'
+import {BrowserRouter, Route,Routes, useNavigate } from 'react-router-dom'
 import {Landing} from './components/Landing'
 import {Dashboard} from './components/Dashboard'
 import './App.css'
@@ -7,16 +7,30 @@ function App() {
 
   return (
     <div>
-      <div style={{backgroundColor:"black", color:"white",textAlign:"center"}}> Header </div>
 
       <BrowserRouter>
+
+        <HeaderComponent></HeaderComponent>
+
         <Routes>
           <Route path='/' element={<Landing />}></Route>
           <Route path='/dashboard' element={<Dashboard />}></Route>
         </Routes>
+
       </BrowserRouter>
+      
     </div>
   )
+}
+
+function HeaderComponent()
+{
+  const navigate=useNavigate();
+  return(
+  <div>
+    <button onClick={()=>navigate("/")}>Landing</button>
+    <button onClick={()=>navigate("/dashboard")}>Dashboard</button>
+  </div>)
 }
 
 export default App
