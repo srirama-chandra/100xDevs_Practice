b7ea7d5170ce7123cc15513f474e73d4d628aeba
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-07-22 11:11:01 +0530

    optimisation

--- Week-7/recoil/src/App.jsx
+++ Week-7/recoil/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil';
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { countAtom } from './store/atoms/count';
 
 
@@ -30,10 +30,10 @@ function CountRender()
 
 function Buttons()
 {
-    const [count,setCount]=useRecoilState(countAtom);
+    const setCount=useSetRecoilState(countAtom);
     return <div>
-      <button onClick={()=>setCount(count+1)}>Increase</button>
-      <button onClick={()=>setCount(count-1)}>Decrease</button>
+      <button onClick={()=>setCount(count => count+1)}>Increase</button>
+      <button onClick={()=>setCount(count => count-1)}>Decrease</button>
     </div>
 }
 
