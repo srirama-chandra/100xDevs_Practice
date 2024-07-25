23913860576cef62514c7018ee44c5582a441a49
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-07-25 11:09:53 +0530

    recoil

--- Week-7/practice/src/App.jsx
+++ Week-7/practice/src/App.jsx
@@ -1,35 +1,44 @@
-import React, { useContext, useState } from "react";
-import { CountContext } from "./components/count";
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { countAtom } from "./store/atoms/count";
 
 function App() {
-  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <CountContext.Provider value={{ count, setCount }}>
-        <Count></Count>
-      </CountContext.Provider>
-    </div>
+      <div>
+          <RecoilRoot>
+            <Count></Count> 
+          </RecoilRoot>
+      </div>
   );
+
 }
 
-function Count() {
-  const { count } = useContext(CountContext);
-  return (
-    <div>
-      {count}
+function Count()
+{
+    return <div>
+      <CountRender></CountRender>
       <Buttons></Buttons>
     </div>
-  );
 }
 
-function Buttons() {
-  const { count, setCount } = useContext(CountContext);
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-    </div>
-  );
+function CountRender()
+{
+    const count= useRecoilValue(countAtom);
+    return <div>{count}</div>
 }
+
+function Buttons()
+{
+    const [count,setCount]=useRecoilState(countAtom);
+  
+    return <div>
+
+      <button onClick={()=>setCount(count+1)}>Increment</button>
+      <button onClick={()=>setCount(count-1)}>Decrement</button>
+
+    </div>
+}
+
+
 
 export default App;
--- Week-7/practice/src/components/count.jsx
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const CountContext=createContext({count:0,setCount:()=>{}});
