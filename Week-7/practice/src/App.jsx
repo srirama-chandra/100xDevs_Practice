54251e786e9f501e4bb3bd981115d82b2030d4e2
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-07-25 09:47:41 +0530

    React-lazy

--- Week-7/practice/src/App.jsx
+++ Week-7/practice/src/App.jsx
@@ -1,28 +1,32 @@
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
-import Landing from "./components/Landing";
-import Dashboard from "./components/Dashboard";
+import React, { Suspense } from "react";
+const Landing = React.lazy(() => import("./components/Landing"));
+const Dashboard = React.lazy(() => import("./components/Dashboard"));
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <ButtonComponent/>
-        <Routes>
-          <Route path="/" element={ <Landing /> }></Route>
-          <Route path="/dashboard" element={ <Dashboard /> }></Route>
-        </Routes>
-      </BrowserRouter>
+      <Suspense fallback={"Loading"}>
+        <BrowserRouter>
+          <ButtonComponent />
+          <Routes>
+            <Route path="/" element={<Landing />}></Route>
+            <Route path="/dashboard" element={<Dashboard />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
 
-function ButtonComponent()
-{
-    const navigate=useNavigate();
-    return <div>
-      <button onClick={()=>navigate("/")}> Landing Page </button>
-      <button onClick={()=>navigate("/dashboard")}> Dashboard Page</button>
+function ButtonComponent() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button onClick={() => navigate("/")}> Landing Page </button>
+      <button onClick={() => navigate("/dashboard")}> Dashboard Page</button>
     </div>
+  );
 }
 
 export default App;
--- Week-7/practice/src/components/Landing.jsx
+++ Week-7/practice/src/components/Landing.jsx
@@ -1,5 +1,3 @@
-
-export default function Landing()
-{
-    return <div>Landing Page</div>
+export default function Landing() {
+  return <div>Landing</div>;
 }
