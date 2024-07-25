9ac072e2a9f3f11bc867c7599939ba87cc2ceb15
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-07-25 09:32:02 +0530

    useNavigateHook

--- Week-7/practice/src/App.jsx
+++ Week-7/practice/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import Landing from "./components/Landing";
 import Dashboard from "./components/Dashboard";
 
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <ButtonComponent/>
         <Routes>
           <Route path="/" element={ <Landing /> }></Route>
           <Route path="/dashboard" element={ <Dashboard /> }></Route>
@@ -13,6 +14,15 @@ function App() {
       </BrowserRouter>
     </div>
   );
+}
+
+function ButtonComponent()
+{
+    const navigate=useNavigate();
+    return <div>
+      <button onClick={()=>navigate("/")}> Landing Page </button>
+      <button onClick={()=>navigate("/dashboard")}> Dashboard Page</button>
+    </div>
 }
 
 export default App;
