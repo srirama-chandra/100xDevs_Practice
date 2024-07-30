455cd7fe5b88f45209ea9f979ebc154d46c51fac
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-07-30 10:45:20 +0530

    flex-wrap

--- Week-8/youtube-clone/src/pages/index.tsx
+++ Week-8/youtube-clone/src/pages/index.tsx
@@ -5,7 +5,22 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-      <div className="flex flex-row">
+      <div className="flex flex-row justify-between flex-wrap">
+            <div className="bg-cyan-500">First</div>
+            <div className="bg-red-500">Second</div>
+            <div className="bg-yellow-500">Third</div>
+            <div className="bg-cyan-500">First</div>
+            <div className="bg-red-500">Second</div>
+            <div className="bg-yellow-500">Third</div>
+            <div className="bg-cyan-500">First</div>
+            <div className="bg-red-500">Second</div>
+            <div className="bg-yellow-500">Third</div>
+            <div className="bg-cyan-500">First</div>
+            <div className="bg-red-500">Second</div>
+            <div className="bg-yellow-500">Third</div>
+            <div className="bg-cyan-500">First</div>
+            <div className="bg-red-500">Second</div>
+            <div className="bg-yellow-500">Third</div>
             <div className="bg-cyan-500">First</div>
             <div className="bg-red-500">Second</div>
             <div className="bg-yellow-500">Third</div>
