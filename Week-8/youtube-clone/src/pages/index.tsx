6fc05c55fcc818a584b0acdde17a776d754a8967
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-07-30 10:39:41 +0530

    flex-direction

--- Week-8/youtube-clone/src/pages/index.tsx
+++ Week-8/youtube-clone/src/pages/index.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-      <div className="flex">
+      <div className="flex flex-row">
             <div className="bg-cyan-500">First</div>
             <div className="bg-red-500">Second</div>
             <div className="bg-yellow-500">Third</div>
