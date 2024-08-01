674ecc8556531a0120950535282032899453a909
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-08-01 11:31:12 +0530

    col-span

--- Week-8/tailwind-practice/src/pages/index.tsx
+++ Week-8/tailwind-practice/src/pages/index.tsx
@@ -5,10 +5,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-      <div className="grid grid-cols-10">
-            <div className="bg-cyan-500 col-span-4">1</div>
-            <div className="bg-red-500 col-span-4">2</div>
-            <div className="bg-yellow-500 col-span-2">3</div>
+      <div className="grid grid-cols-12 gap-4 text-center">
+            <div className="bg-cyan-500 col-span-4 rounded h-10">1</div>
+            <div className="bg-red-500 col-span-4 rounded">2</div>
+            <div className="bg-yellow-500 col-span-4 rounded">3</div>
+            <div className="bg-cyan-500 col-span-8 rounded h-10">1</div>
+            <div className="bg-red-500 col-span-4 rounded">2</div>
+            <div className="bg-cyan-500 col-start-5 col-end-13 rounded h-10">1</div>
       </div>
   );
 }
