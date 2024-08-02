f71c2831c036fe349d81b14d63f9cc7ce8bba9b0
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-08-02 13:16:50 +0530

    grid-row-span

--- Week-8/tailwind-practice/src/pages/index.tsx
+++ Week-8/tailwind-practice/src/pages/index.tsx
@@ -5,13 +5,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-      <div className="grid grid-cols-12 gap-4 text-center">
-            <div className="bg-cyan-500 col-span-4 rounded h-10">1</div>
-            <div className="bg-red-500 col-span-4 rounded">2</div>
-            <div className="bg-yellow-500 col-span-4 rounded">3</div>
-            <div className="bg-cyan-500 col-span-8 rounded h-10">1</div>
-            <div className="bg-red-500 col-span-4 rounded">2</div>
-            <div className="bg-cyan-500 col-start-5 col-end-13 rounded h-10">1</div>
+      <div className="grid grid-rows-3 grid-flow-col gap-2">
+            <div className="bg-cyan-500 rounded row-span-3">1</div>
+            <div className="bg-red-500 rounded">2</div>
+            <div className="bg-yellow-500 rounded row-span-2">3</div>
       </div>
   );
 }
