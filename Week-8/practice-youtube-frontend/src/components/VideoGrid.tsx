c3838b46d60df300902a0a42459173572931f691
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-08-04 10:01:15 +0530

    added-responsiveness

--- Week-8/practice-youtube-frontend/src/components/VideoGrid.tsx
+++ Week-8/practice-youtube-frontend/src/components/VideoGrid.tsx
@@ -70,7 +70,7 @@ export const VideoGrid = () => {
 
     return (
 
-        <div className="grid grid-cols-4 pt-10 gap-3 pl-2 pr-2">
+        <div className="grid grid-cols-1 pt-10 gap-3 pl-2 pr-2 md:grid-cols-2 lg:grid-cols-4">
 
             {
                 VIDEOS.map(video => <VideoComponent key={video.id} title={video.title} image={video.image} author={video.author} logo={video.logo} views={video.views} timeStamp={video.timeStamp} />
