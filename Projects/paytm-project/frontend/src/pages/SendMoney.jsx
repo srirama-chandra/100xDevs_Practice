b6255640a64954f65df47a7e49a98721c18841f2
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-08-12 11:56:02 +0530

    updated-SendMoney-page

--- Projects/paytm-project/frontend/src/pages/SendMoney.jsx
+++ Projects/paytm-project/frontend/src/pages/SendMoney.jsx
@@ -1,8 +1,10 @@
 import { Heading } from "../components/Heading"
-
+import { useState } from "react";
 
 export function SendMoney()
 {
+    const [amount,setAmount]=useState(0);
+
     return <div className="h-screen bg-gray-100 flex justify-center items-center">
 
        <div className="bg-white w-96 p-4 shadow-lg rounded-lg flex flex-col gap-4">
@@ -17,14 +19,13 @@ export function SendMoney()
                 </div>
 
                 <div className="space-y-2 px-4">
-                        <label className="text-sm font-medium ">Amount (in Rs)</label>
-                        <input type={"number"} placeholder={"Enter amount"} className="w-full h-10 border border-input rounded-md px-3 py-2 text-sm focus:ring-0 focus:ring-blue-100"></input>
+                    <label className="text-sm font-medium ">Amount (in Rs)</label>
+                    <input type="number" placeholder="Enter amount" className="w-full h-10 border border-input rounded-md px-3 py-2 text-sm focus:ring-0 focus:ring-blue-100 " onChange={(e) => setAmount(e.target.value)} />
                 </div>
 
                 <div className="px-4 pb-3">
                     <button className="bg-green-500 w-full rounded-md text-white text-sm font-medium px-4 py-2 hover:bg-green-600">Intiate Transfer</button>
                 </div>
-
        </div>
         
     </div>
