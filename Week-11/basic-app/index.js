78929fe906b5fb0971262a3d371fca5a06536fa2
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-09-14 10:43:56 +0530

    basic-app

--- Week-11/basic-app/index.js
+++ Week-11/basic-app/index.js
@@ -7,6 +7,11 @@ app.get("/",(req,res) => {
     return res.status(200).send("Welcome User");
 })
 
+app.get('/welcome',(req,res)=>{
+    return res.status(200).send("Welcome",req.query.name);
+})
+
 app.listen(8080,()=>{
     console.log("Listening On Port",8080);
 });
+
