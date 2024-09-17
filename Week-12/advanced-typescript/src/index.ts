7dcbaf60e1928e99fd5f336b91903b4144ca634e
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-09-17 10:07:02 +0530

    read-only

--- Week-12/advanced-typescript/src/index.ts
+++ Week-12/advanced-typescript/src/index.ts
@@ -1,30 +1,13 @@
 
-// interface User {
-//     id:string;
-//     name:string;
-//     email:string;
-//     password:string;
-// }
-
-// type updatedProps = Pick<User,'name'|'password'>
-
-// type optionalTypes = Partial<updatedProps>
-
-// function updateUser(props:optionalTypes)
-// {
-//     console.log(props.name);
-// }
-
-// updateUser({name:"hi"})
-
-interface Config {
-    url:string;
-    apiKey:string
+type User = {
+    name:string;
+    age:number;
 }
 
-const configData :Readonly<Config> = {
-    url:"randomurl.com",
-    apiKey:"key"
+const userData : Readonly<User> = {
+    name:"User1",
+    age:20,
 }
 
-
+// userData.name= "hello";
+// Error When Trying To Change The Value
