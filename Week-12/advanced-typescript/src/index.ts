5410b75e1c4731a1e108b2f0e6196d268a58a63b
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-09-17 07:29:10 +0530

    read-only

--- Week-12/advanced-typescript/src/index.ts
+++ Week-12/advanced-typescript/src/index.ts
@@ -1,18 +1,30 @@
 
-interface User {
-    id:string;
-    name:string;
-    email:string;
-    password:string;
+// interface User {
+//     id:string;
+//     name:string;
+//     email:string;
+//     password:string;
+// }
+
+// type updatedProps = Pick<User,'name'|'password'>
+
+// type optionalTypes = Partial<updatedProps>
+
+// function updateUser(props:optionalTypes)
+// {
+//     console.log(props.name);
+// }
+
+// updateUser({name:"hi"})
+
+interface Config {
+    url:string;
+    apiKey:string
 }
 
-type updatedProps = Pick<User,'name'|'password'>
-
-type optionalTypes = Partial<updatedProps>
-
-function updateUser(props:optionalTypes)
-{
-    console.log(props.name);
+const configData :Readonly<Config> = {
+    url:"randomurl.com",
+    apiKey:"key"
 }
 
-updateUser({name:"hi"})
+
