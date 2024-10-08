74048d44d9c5c18af55786a62d1c631c846ee861
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-10-08 18:08:06 +0530

    updating-files

--- Week-13/medium-clone/backend/prisma/migrations/20241008054545_added_time/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `date` on the `Blog` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Blog" DROP COLUMN "date",
-ADD COLUMN     "publishedDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
