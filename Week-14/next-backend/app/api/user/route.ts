122890a9168767b7796ae092eff91d5ef5012b64
srirama-chandra <m.sriramachandrachowdari@gmail.com>
2024-10-14 11:08:16 +0530

    basic-next-backend

---
+++ Week-14/next-backend/app/api/user/route.ts
@@ -0,0 +1,5 @@
+
+export function GET()
+{
+    return Response.json({name:"Ram",email:"ram@gmail.com"});
+}
---
+++ Week-14/next-backend/app/page.tsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import Image from "next/image";
+
+async function getUserDetails()
+{
+  const response = await axios.get("http://localhost:3000/api/user");
+  return response.data;
+}
+
+export default async function Home() {
+
+  const userData = await getUserDetails();
+
+  return (
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
+    </div>
+  );
+}
