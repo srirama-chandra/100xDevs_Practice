3b3f67033901c5d52ef383bb37cccbb1e2542781
Turbobot <turbobot@vercel.com>
2025-04-13 10:07:38 +0530

    feat(create-turbo): apply official-starter transform

--- Week-17/paytm-project/apps/web/next.config.js
+++ Week-17/paytm-project/apps/web/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+const withTM = require('next-transpile-modules')(['ui'])
+
+module.exports = withTM({
+  reactStrictMode: true
+})
+
 export default nextConfig;
