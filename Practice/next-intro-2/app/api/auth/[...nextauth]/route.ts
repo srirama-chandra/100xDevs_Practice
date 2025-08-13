dbbbd1493720a05d30f4ce67fa4e3f5dbc671a06
Turbobot <turbobot@vercel.com>
2025-08-13 11:24:50 +0530

    feat(create-turbo): apply official-starter transform

--- Practice/next-intro-2/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import { NEXT_AUTH_CONFIG } from "@/lib/route";
-import NextAuth from "next-auth";
-
-const handler = NextAuth(NEXT_AUTH_CONFIG)
-
-export { handler as GET, handler as POST}
--- Practice/next-intro-2/app/client/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { useSession } from "next-auth/react"
-
-export default function Client()
-{
-    const session = useSession()
-    return <div>{JSON.stringify(session)}</div>
-}
